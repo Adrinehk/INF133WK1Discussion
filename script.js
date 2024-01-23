6f93de0772cc961a9676e924fe70422ed261df77
Adrinehk <147206794+Adrinehk@users.noreply.github.com>
2024-01-22 18:37:24 -0800

    addded interests

--- script.js
+++ script.js
@@ -1,46 +1,20 @@
-var faceImage = document.getElementById('face');
+// var faceImage = document.getElementById('face');
 
-document.getElementById('designerText').addEventListener('mouseenter', function() {
-  faceImage.src = "./imgs/artme.png";
-  faceImage.style.height = "72vh";
-  faceImage.classList.add('face-visible');
-});
-
-document.getElementById('scientistText').addEventListener('mouseenter', function() {
-  faceImage.src = "./imgs/me.png";
-  faceImage.classList.add('face-visible');
-});
-
-const cursor = document.querySelector('.cursor');
-document.addEventListener('mousemove', e=>{
-    cursor.setAttribute("style", "top : "+(e.pageY-12)+"px; left: "+(e.pageX-12)+"px;");
-
-
-
-})
-
-document.addEventListener('click', ()=>{
-    cursor.classList.add("expand");
-    setTimeout(()=>{
-        cursor.classList.remove("expand");
-
-    }, 500)
-})
-const Root = document.querySelector(".about-container");
-const Bg = document.querySelector(".bg");
-
-Root.addEventListener("mousemove", function(e) {
-    Bg.style.left = `${e.clientX}px`;
-    Bg.style.top = `${e.clientY}px`;
-});
-
-// const aboutContainer = document.querySelector(".about-container");
-// const aboutText = document.querySelector("#about p");
-
-// aboutContainer.addEventListener("mousemove", () => {
-//     aboutText.classList.add("light-text");
+// document.getElementById('designerText').addEventListener('mouseenter', function() {
+//   faceImage.src = "./imgs/artme.png";
+//   faceImage.style.height = "72vh";
+//   faceImage.classList.add('face-visible');
 // });
 
-// aboutContainer.addEventListener("mouseleave", () => {
-//     aboutText.classList.remove("light-text");
+// document.getElementById('scientistText').addEventListener('mouseenter', function() {
+//   faceImage.src = "./imgs/me.png";
+//   faceImage.classList.add('face-visible');
+// });
+
+// const Root = document.querySelector(".about-container");
+// const Bg = document.querySelector(".bg");
+
+// Root.addEventListener("mousemove", function(e) {
+//     Bg.style.left = `${e.clientX}px`;
+//     Bg.style.top = `${e.clientY}px`;
 // });
